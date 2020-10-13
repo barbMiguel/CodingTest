2c901eb1f6e28a17ae70f53d6f1ddc6f9dd4b798
Miguel Ángel Osorio Sánchez <miguelangel0699u@gmail.com>
2020-10-13 13:39:57 -0500

    Main challenge complete

--- Prueba/src/Ascensor.java
+++ Prueba/src/Ascensor.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -43,8 +44,6 @@ public class Ascensor {
 		
 
 		try {
-//			cadena = input.readLine();
-//			System.out.println("\n");
 
 			while((cadena = input.readLine()) != null) {
 			contador++;
@@ -54,13 +53,9 @@ public class Ascensor {
 			cadena = cadena.replace(",","");
 			cadena = cadena.replace("{","");
 			cadena = cadena.replace("}","");
-
-//			System.out.println(contador);
-			System.out.println(cadena);
 			
 			control.put(contador,cadena);
 			
-//			cadena = input.readLine();
 			if(contador >=3) {
 				break;
 			}
@@ -69,6 +64,7 @@ public class Ascensor {
 			input.close();
 			entrada.close();
 
+			System.out.println("\n");
 			
 			assignValues(control);
 			isupwards = isUpwards(startingPoint, stops);
@@ -168,51 +164,142 @@ public class Ascensor {
 				upwards = false;
 			}
 		}
-		System.out.println(isupwards);
+		
 		isupwards = upwards;
-		System.out.println(isupwards);
 
 		return upwards;
 	}
 	
-	//This method prints the elevator path
+	/**
+	 * This method prints the elevator path
+	 */
 	public static void elevatorPath() {
 		//If elevator is going upwards this var is 1, otherwise, it will be -1
 		int counting = 1;
 		int currentPosition = startingPoint;
+		int counter = 0;
 		
 		String msjPiso = "Elevador en piso ";
-		String msjDetiene = "Elevador se detiene → ";
+		String msjDetiene = "Elevador se detiene";
 		String msjSubiendo = "Elevador subiendo ";
 		String msjBajando = "Elevador descendiendo ";
 		String msjPisoIngresado = "Piso ingresado → ";
 
-		for (int i = 0; i < stops.size(); i++) {
-			boolean found = false;
-			while((currentPosition <= TOPFLOOR) || (currentPosition > 0) || !found) {
+		for (int i = 0; i <= stops.size(); i++) {
+			boolean found = false;	
+			//indicates if the current position is greater than the index
+			boolean minor = false;
+
+			while((currentPosition <= TOPFLOOR) || (currentPosition > 0) || !found || !minor) {
 				
-				System.out.println(msjPiso + currentPosition);
+				ArrayList<Integer> stopsClon = (ArrayList<Integer>) stops.clone();
+				ArrayList<Integer> stopsOrdered = stopsOrdered(stopsClon);
 				
+				counter++;
+				System.out.println(counter+". "+msjPiso + currentPosition);
+				
+				// The elevator goes upwards
 				if(isupwards) {
-					System.out.println(msjSubiendo);
-				}else {
-					System.out.println(msjBajando);
+					if(stops.size()==1) {
+						i=0;
+					}
+					if(currentPosition > stopsOrdered.get(i)) {
+						counter++;
+						System.out.println(counter+". "+msjSubiendo);
+						minor = true;
+						break;
+					}else if (currentPosition < stopsOrdered.get(i)){
+						counter++;
+						System.out.println(counter+". "+msjSubiendo);
+
+					}else if(currentPosition==stopsOrdered.get(i)) {
+						// Elevator actually reaches a floor of the list
+						found = true;
+						int index = stops.indexOf(currentPosition);
+						stops.remove(index);
+						i -= 1;
+						
+						if(!stops.isEmpty()) {
+							counter++;
+							System.out.println(counter+". "+msjDetiene + " → " + printStops(stops));							
+						}else {
+							counter++;
+							System.out.println(counter+". "+msjDetiene);
+						}
+						
+						//Busca el piso con la clave currentPosition, si ese piso no está en stops
+						// lo debe agregar a la lista de stops
+						if(floorEntered(stops, currentPosition)) {
+							counter++;
+							System.out.println(counter+". "+msjPisoIngresado + printStops(stops));
+						}
+						
+						if(currentPosition==TOPFLOOR) {
+							if(!stops.isEmpty()) {
+								counter++;
+								System.out.println(counter+". "+msjBajando);
+							}
+						}
+						else if(!stops.isEmpty()) {
+							counter++;
+							System.out.println(counter+". "+msjSubiendo);
+						}
+						
+						break;
+					}
+				}
+				// The elevator goes downwards
+				else {
+					if(stops.size()==1) {
+						i=0;
+					}
+					if(currentPosition > stopsOrdered.get(i)) {
+						counter++;
+						System.out.println(counter+". "+msjBajando);
+
+					}else if (currentPosition < stopsOrdered.get(i)){
+						counter++;
+						System.out.println(counter+". "+msjBajando);
+						minor = true;
+						break;
+						
+					}else if(currentPosition==stopsOrdered.get(i)) {
+						// Elevator actually reaches a floor of the list
+						found = true;
+						int index = stops.indexOf(currentPosition);
+						stops.remove(index);
+						i -= 1;
+						
+						if(!stops.isEmpty()) {
+							counter++;
+							System.out.println(counter+". "+msjDetiene + " → " + printStops(stops));							
+						}else {
+							counter++;
+							System.out.println(counter+". "+msjDetiene);
+						}
+						
+						//Busca el piso con la clave currentPosition, si ese piso no está en stops
+						// lo debe agregar a la lista de stops
+						if(floorEntered(stops, currentPosition)) {
+							counter++;
+							System.out.println(counter+". "+msjPisoIngresado + printStops(stops));
+						}
+						
+						if(currentPosition==0) {
+							if(!stops.isEmpty()) {
+								counter++;
+								System.out.println(counter+". "+msjSubiendo);
+							}
+						}else if(!stops.isEmpty()) {
+							counter++;
+							System.out.println(counter+". "+msjBajando);
+						}
+						
+						break;
+					}
 				}
 				
-				// Elevator actually reaches a floor of the list
-				if(currentPosition==stops.get(i)) {
-					found = true;
-					//TODO - Sacar el piso de la lista de paradas
-					System.out.println(msjDetiene + "");
-					
-					//Busca el piso con la clave currentPosition, si ese piso no está en stops
-					// lo debe agregar a la lista de stops
-					if(floorEntered(stops, currentPosition)) {
-						System.out.println(msjPisoIngresado);
-					}
-					
-					break;
-				}
+
 
 				currentPosition += counting;
 
@@ -227,14 +314,85 @@ public class Ascensor {
 				
 			}
 			
+			currentPosition += counting;
+
+			if(currentPosition == 0) {
+				counting = 1;
+				isupwards = true;
+			}else if(currentPosition > TOPFLOOR) {
+				counting = -1;
+				currentPosition += counting -1;
+				isupwards = false;
+			}
+			
+			if(i==stops.size()-1) {
+				i=0;
+			}
 			
 		}
 	}
 
 
-
+	/**
+	 * Returns true if the current stop's destiny has not been entered already to the stops list
+	 * @param stops2
+	 * @param currentPosition
+	 * @return
+	 */
 	private static boolean floorEntered(ArrayList<Integer> stops2, int currentPosition) {
-		// TODO Auto-generated method stub
-		return true;
+		boolean exists = false;
+		if(!stops2.isEmpty()) {
+			for (int i = 0; i < stops2.size(); i++) {
+				if( map.get(currentPosition) == stops2.get(i)) {
+					exists = true;
+				}
+			}
+			if(exists==true) {
+				return false;
+			}else {
+				if(map.get(currentPosition) != null) {
+					stops.add(map.get(currentPosition));
+					return true;
+				}else {
+					return false;
+				}
+			}
+		}else {
+			return false;
+		}
+			
+	}
+	
+	/**
+	 * Formats the output of stops on console
+	 * @param stops2
+	 * @return
+	 */
+	private static String printStops(ArrayList<Integer> stops2) {
+		String msg = "";
+		
+		msg = msg+"[";
+
+		for (int i = 0; i < stops2.size(); i++) {
+			if(i==stops2.size()-1) {
+				msg = msg+(stops2.get(i));
+			}else {
+				msg = msg+(stops2.get(i))+",";
+			}
+		}
+		msg = msg+"]";
+		
+		return msg;
+	}
+	
+	/**
+	 * Orders with sort method the arraylist coming as a parameter
+	 * @param arrayListInt
+	 * @return
+	 */
+	private static ArrayList<Integer> stopsOrdered (ArrayList<Integer> arrayListInt){
+		ArrayList<Integer> ordered = arrayListInt;
+		Collections.sort(ordered);
+		return ordered;
 	}
 }
